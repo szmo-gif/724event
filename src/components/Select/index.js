c9577f9f1f6e7c9ded0c96c78b7a7d0a87d7e20e
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-08-02 16:57:36 +0200

    correction filtre des photos

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,8 +1,5 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useState } from "react";
 import PropTypes from "prop-types";
-
 import "./style.scss";
 
 const Select = ({
@@ -13,13 +10,15 @@ const Select = ({
   label,
   type = "normal",
 }) => {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState(null);
   const [collapsed, setCollapsed] = useState(true);
+
   const changeValue = (newValue) => {
-    onChange();
     setValue(newValue);
-    setCollapsed(newValue);
+    setCollapsed(true);
+    onChange(newValue); // Passer la nouvelle valeur sélectionnée à onChange
   };
+
   return (
     <div className={`SelectContainer ${type}`} data-testid="select-testid">
       {label && <div className="label">{label}</div>}
@@ -31,19 +30,39 @@ const Select = ({
           {!collapsed && (
             <>
               {!titleEmpty && (
-                <li onClick={() => changeValue(null)}>
-                  <input defaultChecked={!value} name="selected" type="radio" />{" "}
-                  Toutes
+                <li>
+                  <button
+                    type="button"
+                    onClick={() => changeValue(null)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter" || e.key === " ") {
+                        changeValue(null);
+                      }
+                    }}
+                  >
+                    <input defaultChecked={!value} name="selected" type="radio" />{" "}
+                    Toutes
+                  </button>
                 </li>
               )}
               {selection.map((s) => (
-                <li key={s} onClick={() => changeValue(s)}>
-                  <input
-                    defaultChecked={value === s}
-                    name="selected"
-                    type="radio"
-                  />{" "}
-                  {s}
+                <li key={s}>
+                  <button
+                    type="button"
+                    onClick={() => changeValue(s)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter" || e.key === " ") {
+                        changeValue(s);
+                      }
+                    }}
+                  >
+                    <input
+                      defaultChecked={value === s}
+                      name="selected"
+                      type="radio"
+                    />{" "}
+                    {s}
+                  </button>
                 </li>
               ))}
             </>
@@ -88,7 +107,7 @@ Select.propTypes = {
   titleEmpty: PropTypes.bool,
   label: PropTypes.string,
   type: PropTypes.string,
-}
+};
 
 Select.defaultProps = {
   onChange: () => null,
@@ -96,6 +115,6 @@ Select.defaultProps = {
   label: "",
   type: "normal",
   name: "select",
-}
+};
 
 export default Select;
